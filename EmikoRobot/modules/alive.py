7592c36e2442417be5832fbbf687dfe5714e29c5
BionXP <112658830+BionXP@users.noreply.github.com>
2023-01-31 11:23:59 +0800

    Update alive.py

--- EmikoRobot/modules/alive.py
+++ EmikoRobot/modules/alive.py
@@ -9,16 +9,16 @@ from EmikoRobot.events import register
 from EmikoRobot import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/951a235ce724cedae5bde.jpg"
+PHOTO = "https://telegra.ph/file/3c653399fb5dad3e68d5c.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Zoids Robot.** \n\n"
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Hinata Robot.** \n\n"
   TEXT += "✘ **I'm Working Properly** \n\n"
-  TEXT += f"✘ **My Master : [sena-ex](https://t.me/excrybaby)** \n\n"
+  TEXT += f"✘ **My Princes : [ʙᴇʟʟᴀ](https://t.me/milkfortunate)** \n\n"
   TEXT += f"✘ **Library Version :** `{telever}` \n\n"
   TEXT += f"✘ **Telethon Version :** `{tlhver}` \n\n"
   TEXT += f"✘ **Pyrogram Version :** `{pyrover}` \n\n"
   TEXT += "**Thanks For Adding Me Here 🔥**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/ZoidsXRobot_bot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/ZoidsXRobotLog")]]
+  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/HinataManage_Bot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/deliciousmilk")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
