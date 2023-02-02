27703934550cc06c5aa59e6ae68b1795f3564587
BionXP <112658830+BionXP@users.noreply.github.com>
2023-02-02 22:44:10 +0800

    Update apakah.py

--- EmikoRobot/modules/apakah.py
+++ EmikoRobot/modules/apakah.py
@@ -23,3 +23,30 @@ async def apakah(event):
         await event.reply('Berikan saya pertanyaan 😐')
         return
     await event.reply(random.choice(APAKAH_STRING))
+    
+    import random
+from EmikoRobot.events import register
+from EmikoRobot import telethn
+
+KAPAN_STRING = ["Besok kali", 
+                 "Kalok ga mager", 
+                 "Kapan Kapan", 
+                 "Nanya Mulu", 
+                 "Tahun Depan", 
+                 "Kalau ga sabtu ya minggu",
+                 "1 Jam lagi",
+                 "Mungkin sekarang",
+                 "8 Jam lagi",
+                 "Sebulan lagi",
+                 "Tahun lalu"
+                 ]
+
+
+@register(pattern="^/kapan ?(.*)")
+async def apakah(event):
+    quew = event.pattern_match.group(1)
+    if not quew:
+        await event.reply('Berikan saya pertanyaan 😐')
+        return
+    await event.reply(random.choice(APAKAH_STRING))
+
