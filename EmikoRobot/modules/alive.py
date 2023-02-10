bc29a74477aa36d99e4bc7d322575dc488e8a8dd
Prem Heroku <124359592+premheroku8@users.noreply.github.com>
2023-02-10 20:15:09 +0800

    Update alive.py

--- EmikoRobot/modules/alive.py
+++ EmikoRobot/modules/alive.py
@@ -9,16 +9,16 @@ from EmikoRobot.events import register
 from EmikoRobot import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/3c653399fb5dad3e68d5c.jpg"
+PHOTO = "https://telegra.ph/file/7f8647c8016634aae1d42.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Hinata Robot.** \n\n"
-  TEXT += "✘ **I'm Working Properly** \n\n"
-  TEXT += f"✘ **My Princes : [ʙᴇʟʟᴀ](https://t.me/milkfortunate)** \n\n"
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), ᴀᴋᴜ ʏᴜɪɪ ʀᴏʙᴏᴛ.** \n\n"
+  TEXT += "✘ **ᴀᴋᴜ ʜɪᴅᴜᴘ ᴅᴀɴ ᴀᴋᴜ ɴʏᴀᴛᴀ** \n\n"
+  TEXT += f"✘ **ᴍʏ ʟᴏʀᴅ : [ ʏᴜɪɪ](https://t.me/onlybionn)** \n\n"
   TEXT += f"✘ **Library Version :** `{telever}` \n\n"
   TEXT += f"✘ **Telethon Version :** `{tlhver}` \n\n"
   TEXT += f"✘ **Pyrogram Version :** `{pyrover}` \n\n"
   TEXT += "**Thanks For Adding Me Here 🔥**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/HinataManage_Bot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/deliciousmilk")]]
+  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/YuiichiroManage_Bot?start=help"), Button.url("ᴜᴘᴅᴀᴛᴇ", "https://t.me/zennihhh")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
